fec8bb8ebd97d08ed05cfc43615c0ae48bda138c
Erayoluwa <157711962+Erayoluwa@users.noreply.github.com>
2024-08-06 10:29:39 +0100

    take out fix

--- backend/routes/menu.js
+++ backend/routes/menu.js
@@ -3,13 +3,11 @@ const sqlite3 = require('sqlite3').verbose();
 const router = express.Router();
 const databasePath = process.env.DATABASE_PATH;
 
-const db = new sqlite3.Database(databasePath);
-
 /**
  * @swagger
  * /menu/create:
  *   post:
- *     summary: Create a new menu
+ *     description: Create a new menu
  *     requestBody:
  *       required: true
  *       content:
@@ -28,29 +26,18 @@ const db = new sqlite3.Database(databasePath);
  *     responses:
  *       201:
  *         description: Menu created successfully
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 id:
- *                   type: integer
- *                   description: The ID of the created menu
- *                   example: 1
+ *       400:
+ *         description: Bad request
  *       500:
  *         description: Server error
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 error:
- *                   type: string
- *                   description: Error message
- *                   example: "Could not create menu"
  */
 router.post('/create', (req, res) => {
     const { name, take_out } = req.body;
+    if (!name || take_out === undefined) {
+        return res.status(400).json({ error: "Name and take_out flag are required." });
+    }
+
+    let db = new sqlite3.Database(databasePath);
     const query = 'INSERT INTO Menu (name, take_out) VALUES (?, ?)';
 
     db.run(query, [name, take_out], function(err) {
@@ -58,6 +45,12 @@ router.post('/create', (req, res) => {
             return res.status(500).json({ error: err.message });
         }
         res.status(201).json({ id: this.lastID });
+    });
+
+    db.close((err) => {
+        if (err) {
+            console.error('Error closing database connection:', err.message);
+        }
     });
 });
 
