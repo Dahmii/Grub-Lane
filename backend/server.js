bc9893218ad26f5485a79cdb59a05f36e3973017
Ireoluwa Fakeye <ireoluwa.fakeye@rothamsted.ac.uk>
2024-08-08 19:38:34 +0100

    fixes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
-const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
-const cors = require("cors"); 
+const fs = require('fs');
+const https = require('https');
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -11,12 +13,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(cors()); 
+app.use(cors());
 
-const initializeDatabase = require("./initializeDatabase");
-const userRoutes = require("./routes/users");
-const orderRoutes = require("./routes/orders");
-const reservationRoutes = require("./routes/reservations");
+const initializeDatabase = require('./initializeDatabase');
+const userRoutes = require('./routes/users');
+const orderRoutes = require('./routes/orders');
+const reservationRoutes = require('./routes/reservations');
 const menuRoutes = require('./routes/menu');
 const dishRoutes = require('./routes/dish');
 
@@ -25,27 +27,32 @@ initializeDatabase();
 const swaggerOptions = {
   swaggerDefinition: {
     info: {
-      title: "API Documentation",
-      description: "API Information",
-      version: "1.0.0",
+      title: 'API Documentation',
+      description: 'API Information',
+      version: '1.0.0',
       contact: {
-        name: "API Support",
+        name: 'API Support',
       },
-      servers: ["http://localhost:3000"],
+      servers: ['http://localhost:3000'],
     },
   },
-  apis: ["./routes/users.js", "./routes/orders.js", "./routes/reservations.js", "./routes/dish.js", "./routes/menu.js"],
+  apis: ['./routes/users.js', './routes/orders.js', './routes/reservations.js', './routes/dish.js', './routes/menu.js'],
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-app.use("/users", userRoutes);
-app.use("/orders", orderRoutes);
-app.use("/reservations", reservationRoutes);
+app.use('/users', userRoutes);
+app.use('/orders', orderRoutes);
+app.use('/reservations', reservationRoutes);
 app.use('/menu', menuRoutes);
 app.use('/dish', dishRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://0.0.0.0:${port}`);
+const options = {
+  key: fs.readFileSync('ssl/server.key'),
+  cert: fs.readFileSync('ssl/server.cert')
+};
+
+https.createServer(options, app).listen(port, () => {
+  console.log(`Server is running on https://0.0.0.0:${port}`);
 });
