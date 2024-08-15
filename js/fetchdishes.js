dd822a5e7fdc0f0bea4a14780ff2518ac2d29dad
Dami Akindele <114703098+Dahmii@users.noreply.github.com>
2024-08-15 22:38:12 +0100

    update

--- js/fetchdishes.js
+++ js/fetchdishes.js
@@ -1,15 +1,11 @@
 function fetchMenuData(menuType) {
   const takeOut = menuType === "dine_in" ? "false" : "true";
-<<<<<<< HEAD
   const endpointUrl = `https  ://grublanerestaurant.com:3000/dish/getDishes?take_out=${takeOut}`;
-=======
-  const endpointUrl = `https://grublanerestaurant.com/api/dish/getDishes?take_out=${takeOut}`;
->>>>>>> bbcd0c0b49294f9fb57f134be4ab83bd16872ee5
 
   fetch(endpointUrl)
     .then((response) => {
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       return response.json();
     })
@@ -32,7 +28,9 @@ function fetchMenuData(menuType) {
             menuHtml += `
                 <div class="col-md-4 mb-4">
                   <div class="menu-item">
-                    <img src="${item.image_url}" alt="${item.name}" class="img-fluid" />
+                    <img src="${item.image_url}" alt="${
+              item.name
+            }" class="img-fluid" />
                     <h4>${item.name}</h4>
                     <p>N${item.price}</p>
                     ${
@@ -76,7 +74,9 @@ function addCartFunctionality() {
     button.addEventListener("click", function () {
       const itemName = this.getAttribute("data-item");
       const itemPrice = parseInt(this.getAttribute("data-price"));
-      const existingItemIndex = cart.findIndex((item) => item.name === itemName);
+      const existingItemIndex = cart.findIndex(
+        (item) => item.name === itemName
+      );
 
       if (existingItemIndex > -1) {
         cart[existingItemIndex].quantity += 1;
