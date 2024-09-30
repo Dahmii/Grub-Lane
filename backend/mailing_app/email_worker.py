fd44a990c452e768a875575d263ae1fb2a6042b3
Ireoluwa Fakeye <ireoluwa.fakeye@rothamsted.ac.uk>
2024-09-30 21:07:51 +0100

    email sending logic

--- backend/mailing_app/email_worker.py
+++ backend/mailing_app/email_worker.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_client = redis.StrictRedis(host='localhost', port=6379)
 
 SMTP_SERVER = os.getenv('SMTP_SERVER')
 SMTP_PORT = int(os.getenv('SMTP_PORT', 587))
